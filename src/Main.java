6eb045cf2d0ad5bf280922c099cfb4c46cdd90fc
przemyslawren <przemyslaw.ren@gmail.com>
2024-03-24 11:28:42 +0100

    add fetch city weather

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Service s = new Serviceg("Poland");
+        Service s = new Service("Poland");
         String weatherJson = s.getWeather("Warsaw");
-        Double rate1 = s.getRateFor("USD");
-        Double rate2 = s.getNBPRate();
+        System.out.println(weatherJson);
+//        Double rate1 = s.getRateFor("USD");
+//        Double rate2 = s.getNBPRate();
         // ...
         // część uruchamiająca GUI
     }
