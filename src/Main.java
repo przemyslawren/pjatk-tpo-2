608ee4f36590d3db9a0b91696e407fea76fb40f4
przemyslawren <przemyslaw.ren@gmail.com>
2024-04-10 19:39:53 +0200

    add all the services

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
         Service s = new Service("Poland");
         String weatherJson = s.getWeather("Warsaw");
         System.out.println(weatherJson);
         Double rate1 = s.getRateFor("USD");
-//        Double rate2 = s.getNBPRate();
+        Double rate2 = s.getNBPRate();
         System.out.println(rate1);
+        System.out.println(rate2);
         // ...
         // część uruchamiająca GUI
+        Application.launch(View.class);
     }
 }
--- src/Service.java
+++ src/Service.java
@@ -1,25 +1,99 @@
+
 import java.io.IOException;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
+import java.util.Currency;
+import java.util.Locale;
+import org.json.JSONObject;
 
 public class Service {
-    Country country;
+    private final Locale locale;
+
     Service(String country) {
-        this.country = new Country();
+        this.locale = getLocaleFromName(country);
+        System.out.println(locale);
     }
 
-    // zwraca informację o pogodzie w podanym mieście danego kraju w formacie JSON (to ma być pełna informacja
-    // uzyskana z serwisu openweather - po prostu tekst w formacie JSON)s
+    private Locale getLocaleFromName(String country) {
+        country = country.replaceAll("\\s", "").toLowerCase();
+
+        for (Locale locale : Locale.getAvailableLocales())
+            if (country.equals(locale.getDisplayCountry(Locale.ENGLISH).toLowerCase()))
+                return locale;
+        return null;
+    }
+
     String getWeather(String city) {
         String weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
         String apiKey = "3bf47a4cc9cb78dce3f01b190307fb6b";
-        String apiUrl = "&appid=" + apiKey;
-        String encodedCity = URLEncoder.encode(city, StandardCharsets.UTF_8);
-        String fullUrl = weatherUrl + encodedCity + apiUrl;
+        String fullUrl = weatherUrl + city + "," + locale.getCountry() + "&appid=" + apiKey;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(fullUrl))
+                .GET()
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject obj = new JSONObject(response.body());
+            double temperatureInKelvin = obj.getJSONObject("main").getDouble("temp");
+            int temperatureInCelsius = (int) (temperatureInKelvin - 273.15);
+//
+//            String weatherDescription = obj
+//                    .getJSONArray("weather")
+//                    .getJSONObject(0)
+//                    .getString("description");
+
+//            return temperatureInCelsius + "°C " + weatherDescription;
+
+            return response.body();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "Data not found";
+        }
+    }
+
+    // zwraca kurs waluty danego kraju wobec waluty podanej jako argument
+    Double getRateFor(String currency) {
+        String fullUrl = "https://open.er-api.com/v6/latest/" + currency;
+        Double rate = null;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(fullUrl))
+                .GET()
+                .build();
+        HttpResponse<String> response;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject obj = new JSONObject(response.body());
+
+            Double selectedCurrency = obj.getJSONObject("rates")
+                    .getDouble(currency);
+
+            Double countryCurrency = obj.getJSONObject("rates")
+                    .getDouble(String.valueOf(Currency.getInstance(locale)));
+
+            rate = selectedCurrency/countryCurrency;
+
+            return rate;
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return rate;
+        }
+    }
+
+    // zwraca kurs złotego wobec waluty danego kraju
+    Double getNBPRate() {
+        String fullUrl = "https://open.er-api.com/v6/latest/PLN";
+        Double rate = null;
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -27,49 +101,24 @@ public class Service {
                 .GET()
                 .build();
 
+        HttpResponse<String> response;
+
         try {
-            HttpResponse<String> response =
-                    client.send(request, HttpResponse.BodyHandlers.ofString());
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject obj = new JSONObject(response.body());
 
-            if (response.statusCode() == 200) {
-                return response.body();
-            } else {
-                throw new RuntimeException(
-                        "Failed to fetch weather data: HTTP Response code: " + response.statusCode());
-            }
+            Double PLN = obj.getJSONObject("rates")
+                    .getDouble("PLN");
+            Locale locale = Locale.getDefault();
+            Double countryCurrency = obj.getJSONObject("rates")
+                    .getDouble(String.valueOf(Currency.getInstance(locale)));
+
+            rate = PLN/countryCurrency;
+            return rate;
+
         } catch (IOException | InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("Error fetching weather data");
+            e.printStackTrace();
+            return rate;
         }
     }
-        // zwraca kurs waluty danego kraju wobec waluty podanej jako argument
-        Double getRateFor(String kod_waluty){
-            String exchangeRateUrl = "https://open.er-api.com/v6/latest/";
-            String encodedCurrency = URLEncoder.encode(kod_waluty, StandardCharsets.UTF_8);
-            String fullUrl = exchangeRateUrl + encodedCurrency;
-
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(fullUrl))
-                    .GET()
-                    .build();
-            try {
-                HttpResponse<String> response =
-                        client.send(request, HttpResponse.BodyHandlers.ofString());
-
-                if (response.statusCode() == 200) {
-                    return Double.valueOf(response.body());
-                } else {
-                    throw new RuntimeException(
-                            "Failed to fetch currency data: HTTP Response code: " + response.statusCode());
-                }
-            } catch (IOException | InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException("Error fetching currency data");
-            }
-        }
-        // zwraca kurs złotego wobec waluty danego kraju
-        private Double getNBPRate () {
-            return 4.25;
-        }
-    }
+}
