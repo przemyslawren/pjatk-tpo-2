345bfc580ca46f15cd263b0e081e0a9be727d84e
przemyslawren <przemyslaw.ren@gmail.com>
2024-04-12 23:50:54 +0200

    add web scraping

--- src/Service.java
+++ src/Service.java
@@ -5,9 +5,16 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Currency;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Service {
     private Locale locale;
@@ -95,34 +102,52 @@ public class Service {
         Currency currency = Currency.getInstance(locale);
         String currencyCode = currency.getCurrencyCode();
 
-        String fullUrl = "https://open.er-api.com/v6/latest/PLN";
-        double rate;
+        String[] urls = {
+                "https://static.nbp.pl/dane/kursy/xml/a073z240412.xml",
+                "https://static.nbp.pl/dane/kursy/xml/b015z240410.xml",
+                "https://static.nbp.pl/dane/kursy/xml/c073z240412.xml"
+        };
 
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(fullUrl))
-                .GET()
-                .build();
+        Map<String, Double> currencyMap = new HashMap<>();
 
-        HttpResponse<String> response;
+        for (String url : urls) {
+            try {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(url))
+                        .GET()
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
 
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            JSONObject obj = new JSONObject(response.body());
-
-            Double PLN = obj.getJSONObject("rates")
-                    .getDouble("PLN");
-
-            Double countryCurrencyRate = obj.getJSONObject("rates").getDouble(currencyCode);
-            rate = PLN / countryCurrencyRate;
-
-            return rate;
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+                currencyMap.putAll(parseNBPResponse(response.body()));
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        if (currencyCode.equals("PLN")) {
+            return 1.0;
+        }
+
+        return currencyMap.get(currencyCode);
+    }
+
+    static Map<String, Double> parseNBPResponse(String xmlData) {
+        Document doc = Jsoup.parse(xmlData, "", org.jsoup.parser.Parser.xmlParser());
+        Elements positions = doc.select("pozycja");
+        Map<String, Double> currencyRates = new HashMap<>();
+
+        for (Element position : positions) {
+            String currencyCode = position.select("kod_waluty").text();
+            String rateString = position.select("kurs_sredni").text()
+                    .replace(',', '.');
+
+            if (rateString.contains(".")) {
+                Double exchangeRate = Double.parseDouble(rateString);
+
+                currencyRates.put(currencyCode, exchangeRate);
+            }
+        }
+        return currencyRates;
     }
 }
